baa15e646a6ef755bad23d12afb3e6fe4b5286bf
Hyemm <hmlee5938@gmail.com>
2022-08-11 00:15:54 +0900

    주석정리

--- public-transport/src/Scenario.java
+++ public-transport/src/Scenario.java
@@ -4,37 +4,26 @@ public class Scenario {
         scenario.TaxiScen();
         scenario.BusScen();
     }
+    //### Bus
     public void BusScen(){
-        //### Bus
-        //
         //1. **Bus** - 2대 생성
         Bus bus1 = new Bus(), bus2 = new Bus();
         //**※ Bus 1대로 진행**
-        //
-        //1. **Bus**
-        //    1.  승객 +2
+        //1. **Bus**: 승객 +2
         bus1.boarding(2);
-        //3. **Bus**
-        //    1. 주유량 -50
+        //3. **Bus**: 주유량 -50
         bus1.refuel(-50);
-        //5. **Bus - 상태 변경**
-        //    1. 차고지행
+        //5. **Bus - 상태 변경** : 차고지행
         bus1.ChangeState();
-        //6. **Bus**
-        //    1. 주유량 +10
+        //6. **Bus**: 주유량 +10
         bus1.refuel(10);
-        //8. **Bus -** **상태 변경**
-        //    1. 운행중
+        //8. **Bus -** **상태 변경**: 운행중
         bus1.ChangeState();
-        //9. **Bus**
-        //    1. 승객 +45
+        //9. **Bus**: 승객 +45
         bus1.boarding(45);
-        //11. **Bus**
-        //    1. 승객 +5
+        //11. **Bus**: 승객 +5
         bus1.boarding(5);
-
-        //13. **Bus**
-        //    1. 주유량 -55
+        //13. **Bus**:주유량 -55
         bus1.refuel(-55);
     }
     //### Taxi
@@ -42,27 +31,17 @@ public class Scenario {
         //1. **Taxi** - 2대 생성
         Taxi taxi1 = new Taxi(), taxi2 = new Taxi();
         //**※ Taxi 1대로 진행**
-        //1. **Taxi**
-        //    1.  승객 +2
-        //    2. 목적지 = 서울역
-        //    3. 목적지까지 거리 = 2km
+        //1. **Taxi**: 승객 +2, 목적지 = 서울역, 목적지까지 거리 = 2km
         taxi1.boarding(2,"서울역",2);
-
-        //3. **Taxi**
-        //    1. 주유량 -80
+        //3. **Taxi**: 주유량 -80
         taxi1.refuel(-80);
         //4. **Taxi -** **요금 결제**
         taxi1.FarePayment();
-        //6. **Taxi**
-        //    1. 승객 +5
+        //6. **Taxi**: 승객 +5
         taxi1.boarding(5,"",0);
-        //8. **Taxi**
-        //    1. 승객 +3
-        //    2. 목적지 = 구로디지털단지역
-        //    3. 목적지까지 거리 = 12km
+        //8. **Taxi**: 승객 +3, 목적지 = 구로디지털단지역, 목적지까지 거리 = 12km
         taxi1.boarding(3,"구로디지털단지역",12);
-        //10. **Taxi**
-        //    1. 주유량 -20
+        //10. **Taxi**: 주유량 -20
         taxi1.refuel(-20);
         //11. **Taxi - 요금 결제**
         taxi1.FarePayment();
